f98e0f5665f1e8a44ed31b1c4c8efd9724cc82f8
Christopher <cgtsompa@gmail.com>
2022-11-25 17:45:42 +0000

    nav btn + d-applications changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
-menuToggler.addEventListener('click', ev => {
+menuToggler.addEventListener('click', (ev) => {
   menu.classList.toggle('open');
-  menuToggler.textContent = menuToggler.textContent === "X" ? "≡" : "X";
+  menuToggler.textContent = menuToggler.textContent === 'X' ? '≡' : 'X';
 });
 
-const search = document.querySelector("input");
-const form = document.querySelector("form");
-const searchResult = document.querySelector(".results");
-const errorMsg = document.querySelector(".alert");
+const search = document.querySelector('input');
+const form = document.querySelector('form');
+const searchResult = document.querySelector('.results');
+const errorMsg = document.querySelector('.alert');
 // const line = document.querySelector("hr");
 const apiURL =
-  "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=";
+  'https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=';
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   let searchValue = search.value;
-  if(searchValue === ""){
-    errorMessage("Search cannot be empty. Please enter your search.");
+  if (searchValue === '') {
+    errorMessage('Search cannot be empty. Please enter your search.');
   } else {
     getResult(searchValue);
   }
@@ -26,32 +26,31 @@ async function getResult(searchVal) {
   const results = await response.json();
 
   console.log(results);
-  if(results.query.search.length == 0){
-    return errorMessage("Invalid search, please enter another search");
+  if (results.query.search.length == 0) {
+    return errorMessage('Invalid search, please enter another search');
   } else {
     displayData(results);
   }
 }
 
-function errorMessage(msg){
-  errorMsg.style.display = "block";
+function errorMessage(msg) {
+  errorMsg.style.display = 'block';
   // line.style.display = "block";
 
   errorMsg.textContent = msg;
 }
 
-function displayData(results){
+function displayData(results) {
   // line.style.display = "block";
-  let output = "";
+  let output = '';
   results.query.search.forEach((result) => {
     let resultURL = `https://en.wikipedia.org/?curid=${result.pageid}`;
-    output +=
-    `   <div class="result">
+    output += `   <div class="result">
           <a href="${resultURL}">${result.title}</a>
           <br/>
           <p>${result.snippet}</p>
         </div>
         `;
-        searchResult.innerHTML = output;
+    searchResult.innerHTML = output;
   });
 }
